d6024e01b46fdb57162568f9049eb1a259b25ccf
gcxfd <i@rmw.link>
2021-07-11 02:41:53 +0800

    .

--- train/train.sh
+++ train/train.sh
@@ -6,6 +6,10 @@ _DIR=$(dirname $(realpath "$0"))
 
 cd $_DIR
 
+if [ ! -d "sdb" ] ; then
+git clone git@github.com:rmw-link/sdb.git --depth 1
+fi
+
 cargo build --release
 #export RAYON_NUM_THREADS=8
 RUST_LOG="sanakirja=debug" RUST_BACKTRACE=1 target/release/train
